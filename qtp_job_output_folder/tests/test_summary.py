3a3a8765202cb59d860064a1c8970eea77f810d8
Antonio Gonzalez <antgonza@gmail.com>
2023-01-13 07:52:26 -0700

    adding support for new /qiita_db/artifacts

--- qtp_job_output_folder/tests/test_summary.py
+++ qtp_job_output_folder/tests/test_summary.py
@@ -61,7 +61,8 @@ class SummaryTests(PluginTestCase):
         # asserting content of html
         res = self.qclient.get("/qiita_db/artifacts/%s/" % aid)
         # cleaning artifact files, to avoid errors
-        [self._clean_up_files.extend(f) for f in res['files'].values()]
+        [self._clean_up_files.extend(f['filepath'])
+         for f in res['files'].values()]
         html_fp = res['files']['html_summary'][0]
         self._clean_up_files.append(html_fp)
         with open(html_fp) as html_f:
